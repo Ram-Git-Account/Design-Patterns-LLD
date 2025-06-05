765bd0fe3631cbaa7f55fa73790e292464f5523f
Ram-Git-Account <sairamkrishna350@gmail.com>
2025-06-05 13:20:52 +0530

    Strategy Design Pattern

---
+++ Strategy/Client.java
@@ -0,0 +1,15 @@
+package Strategy;
+
+public class Client {
+    public static void main(String[] args) {
+        Maps maps = new Maps();
+        maps.getPath("Patna", "Delhi", TransportationMode.WALK);
+    }
+}
+/*
+    Created an interface with method for implementation
+    Created the classes with the actual impl
+    Optional -> Make the classes singleton
+    Factory class to get object of actual impl class
+ */
+
---
+++ Strategy/TransportationMode.java
@@ -0,0 +1,5 @@
+package Strategy;
+
+public enum TransportationMode {
+    CAR, BIKE, WALK;
+}
